3a215d22ff19e7657f58f764e843b9b06f88b05c
Ahmad Fauzy <116853020+ahmadfauzy19@users.noreply.github.com>
2023-08-25 21:03:00 +0700

    Add files via upload

---
+++ Soal1.java
@@ -0,0 +1,21 @@
+package com.mycompany.pbo_p1;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal1 {
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte)(angka1+angka2);
+        
+        System.out.println(hasil);
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pbo_p1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
